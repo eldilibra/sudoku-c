9ffea66e57efd564a62d6045644aa6be6cc3f380
eldilibra <writeme@ldlibra.com>
2014-01-07 16:48:09 -0600

    getting started

---
+++ sudoku.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+int main (void) {
+  srand(time(NULL)/2);
+  int game[9][9];
+  printf("Hello world %d\n", game[0][0]);
+  int test_rand = rand() % 9 + 1;
+  printf("Rand test %d\n", test_rand);
+  return 0;
+}
